a1e5ef8a28d7b2f2db70fac19ffc5653472d9266
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-01-27 16:00:27 -0700

    set up appJS and weatherJS to call to API and retrieve data wired up properly

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * JavaScript
 * Bootswatch
 * Bootstrap
+* Weatherbit API
---
+++ weather.js
@@ -0,0 +1,22 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = "e30050b8ed1046c89a9ecaf24e5cb56b";
+        this.city = city;
+        this.state = state;
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`https://api.weatherbit.io/v2.0/current?city=${this.city}&state=${this.state}&key=${this.apiKey}`);
+
+        const responseData = await response.json();
+
+        return responseData.data;
+    }
+
+    // Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
