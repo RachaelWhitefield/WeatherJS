d0a7cbb0f326171cfe8161797a210c470f143ef7
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-01-29 15:19:46 -0700

    got data to persist to local storage

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init weather object
-const data = new Weather("Denver", "CO");
+const data = new Weather(weatherLocation.city, weatherLocation.state);
 // Init UI
 const ui = new UI();
 
@@ -11,7 +15,11 @@ document.getElementById("w-change-btn").addEventListener("click", (e) => {
     const city = document.getElementById("city").value;
     const state = document.getElementById("state").value;
 
-    data.changeLocation("San Diego", "CA");
+    // Change location
+    data.changeLocation(city, state);
+
+    // Set location in local storage
+    storage.setLocationData(city,state);
 
     // Get and display weather
     getWeather();
